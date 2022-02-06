6e4707190f3bed3dc78274c1df22ad6ef06fb098
Hien Vo <hienvo310799@gmail.com>
2022-02-06 14:27:04 +0700

    update

---
+++ sb8_ver_demo/src/main/java/com/example/sb8_ver_demo/Sb8VerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb8_ver_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb8VerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb8VerDemoApplication.class, args);
+	}
+
+}
---
+++ sb8_ver_demo/src/test/java/com/example/sb8_ver_demo/Sb8VerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb8_ver_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb8VerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
