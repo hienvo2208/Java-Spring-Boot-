f08bfa6a16dd9f4cc5c44eae128a1a18a4da1eca
Hien Vo <hienvo310799@gmail.com>
2022-02-07 12:20:26 +0700

    update

--- sb8_ver_demo/src/main/java/com/example/sb8_ver_demo/WebController.java
+++ sb8_ver_demo/src/main/java/com/example/sb8_ver_demo/WebController.java
@@ -1,7 +1,9 @@
 package com.example.sb8_ver_demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -10,12 +12,23 @@ public class WebController {
     public String index() {
         return "index";
     }
+
     @GetMapping("/about")
-    public String about(){
+    public String about() {
         return "about";
     }
+
     @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+    public String hello(
+            // Request param "name" sẽ được gán giá trị vào biến String.
+            @RequestParam(name = "name", required = false, defaultValue = "") String name,
+            // Model là một object của spring Boot, được gán vào mọi request.
+            Model model) {
+        // Gán vào model giá trị name nhận được
+        model.addAttribute("name", name);
+        return "hello"; // trả về file hello.html cùng với thông tin trong object Model.
+
     }
+
+
 }
